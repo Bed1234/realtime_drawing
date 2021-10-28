cbfd9ca6471e05cd0bc852884444215aba9bcb4e
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-10-28 18:44:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
+
+
+nose_X = 0;
+nose_Y = 0;
+difference = 0;
+
+leftWristX = 0;
+rightWristX = 0;
+
+
 function setup() {
     video = createCapture(VIDEO);
-    video.size(500,600);
+    video.size(500,500);
     video.position(10,60);
 
 
-    canvas =  createCanvas(550,450);
-    canvas.position(600,120);
+    canvas =  createCanvas(550,500);
+    canvas.position(600,75);
 
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -20,6 +30,16 @@ function gotPoses(results) {
     if (results.length > 0) 
     {
          console.log(results);
+         nose_X = results[0].pose.nose.x;
+         nose_Y = results[0].pose.nose.y;
+         console.log(" nose_X = " + nose_X  +  " nose_Y = " + nose_Y);
+
+         leftWristX = results[0].pose.leftWrist.x;
+         rightWristX = results[0].pose.rightWrist.x;
+
+         difference = floor(leftWristX - rightWristX);
+         console.log(" left wrist = " + leftWristX + " right wrist = " + rightWristX + " difference =  " +  difference);
+
     } 
         
     
@@ -32,4 +52,9 @@ function modelLoaded() {
 
 function draw(){
   background('#FFFF00');
+
+  fill ('#0000FF');
+  stroke ('#0000FF');
+  square (nose_X , nose_Y , difference);
+
 }
